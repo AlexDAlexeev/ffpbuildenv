e92590c96965574e3af3ff1e5c3e50a0f93fab8f
danieleotti@gmail.com <danieleotti@gmail.com>
2011-10-13 16:28:03 +0000

    Corrected package creation to skip svn CSV git files
    git-svn-id: http://ffp-additional.googlecode.com/svn/trunk@22 0345c17c-9144-d91a-066e-c01cc2519829

--- source/Conf.d/makepkg.sh
+++ source/Conf.d/makepkg.sh
@@ -10,4 +10,4 @@ EOF
 [ $# -eq 1 ] || die "Usage: $0 <package-file>"
 [ -e "$1" ] && rm -f "$1"
 mkdir -p $(dirname "$1")
-tar cf - --exclude=\*~ . | gzip -c -9 >$1.tgz
+tar cf - --exclude=.svn --exclude=.git --exclude=CVS --exclude=\*~ . | gzip -c -9 >$1.tgz
