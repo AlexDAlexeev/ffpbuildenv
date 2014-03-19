ce9706c1e69a8f729e2ae3cba94de52161108249
Darkvater <tamas.farago@credit-suisse.com>
2014-03-19 13:11:25 +0800

    Make package creation verbose so we can see its progress
    Especially xz compression can be quite slow on the nas
    at least we have an idea where we are and how much more
    needs to be done

--- source/Conf.d/makepkg.sh
+++ source/Conf.d/makepkg.sh
@@ -10,5 +10,5 @@ EOF
 [ $# -eq 1 ] || die "Usage: $0 <package-file>"
 [ -e "$1" ] && rm -f "$1"
 mkdir -p $(dirname "$1")
-tar cf - --exclude=.svn --exclude=.git --exclude=CVS --exclude=\*~ . | gzip -c -9 >$1.tgz
-tar cf - --exclude=.svn --exclude=.git --exclude=CVS --exclude=\*~ . | xz -c >$1.txz
+tar cf - --exclude=.svn --exclude=.git --exclude=CVS --exclude=\*~ . | gzip -cv -9 >$1.tgz
+tar cf - --exclude=.svn --exclude=.git --exclude=CVS --exclude=\*~ . | xz -cv >$1.txz
