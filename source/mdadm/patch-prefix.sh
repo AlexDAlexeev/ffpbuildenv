9621ee0910696cf883e0acdf093f6261da3fc404
Darkvater <tamas.farago@credit-suisse.com>
2014-03-21 00:18:27 +0800

    New package mdadm
    mdadm is a tool for managing Linux Software RAID arrays.
    
    It can create, assemble, report on, and monitor arrays.
    
    It can also move spares between raid arrays when needed.

---
+++ source/mdadm/patch-prefix.sh
@@ -0,0 +1,8 @@
+sed -i \
+-e "/^INSTALL/ s@/usr/bin/install@install@" \
+-e "/^SYSCONFDIR/ s@/etc@${CPREFIX}/etc@" \
+-e "/^SYSTEMD_DIR/ s@/lib@${CPREFIX}/lib@" \
+-e "/^RUN_DIR/ s@/run/mdadm@/dev/.mdadm@" \
+-e "/^CXFLAGS/ s@-ggdb@-I/ffp/include/ -ggdb@" \
+Makefile
+           
