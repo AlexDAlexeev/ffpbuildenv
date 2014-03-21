543d36e71fe7aafb921836db4b4e236342417820
Darkvater <tamas.farago@credit-suisse.com>
2014-03-21 23:41:24 +0800

    Fix kernel headers package
    Kernel header package wasn't using all includes in the package
    causing some applications not able to compile

--- source/kernel-headers/destdir.sh
+++ source/kernel-headers/destdir.sh
@@ -1,1 +1,9 @@
 make INSTALL_HDR_PATH=$D$CPREFIX headers_install
+
+cp -an arch/arm/include/asm $D$CPREFIX/include/asm
+cd include
+cp -an linux scsi sound asm-generic $D$CPREFIX/include
+
+cd $D
+chown -R root:root *
+
