f8288b9b5637e3afe1d885140d0ee88c427a2edf
Darkvater <tamas.farago@credit-suisse.com>
2014-03-21 23:40:06 +0800

    Use $CSYSCONFDIR for /etc instead of hardcoding it

--- source/mdadm/patch-prefix.sh
+++ source/mdadm/patch-prefix.sh
@@ -1,6 +1,6 @@
 sed -i \
 -e "/^INSTALL/ s@/usr/bin/install@install@" \
--e "/^SYSCONFDIR/ s@/etc@${CPREFIX}/etc@" \
+-e "/^SYSCONFDIR/ s@/etc@${CSYSCONFDIR}@" \
 -e "/^SYSTEMD_DIR/ s@/lib@${CPREFIX}/lib@" \
 -e "/^RUN_DIR/ s@/run/mdadm@/dev/.mdadm@" \
 -e "/^CXFLAGS/ s@-ggdb@-I/ffp/include/ -ggdb@" \
